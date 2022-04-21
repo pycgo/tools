2539af33d65bb49ec331fcc1b2a36cd4decf02ea
pycgo <55949132+pycgo@users.noreply.github.com>
2022-04-21 17:13:05 +0800

    增加pip库的 web展示

--- tools.py
+++ tools.py
@@ -10,13 +10,20 @@ def index():
             if "dnf install" in line:
                 toolslist = line.split(' ')
     data_list = toolslist[3:][:-4]
-    meta = "已经安装的工具列表如下：\n"
-    str_tools = meta + '\n'.join(data_list)
+    meta_linux = "1、已经安装的工具列表如下：\n"
+    str_tools = meta_linux + '\n'.join(data_list)
+
+    meta_python3 = "\n\n\n2、python3.8的第三方库如下：\n"
+    pip_list = []
+    with open('requirements.txt','r') as f:
+        for line in f:
+            pip_list.append(line)
+    str_tools = str_tools + meta_python3 + ''.join(pip_list)
     print(str_tools)
+
     return str_tools
 
 index()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
-
